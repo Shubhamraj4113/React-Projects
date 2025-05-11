4c21d287ec60ad5a49ae6ccbd94248c18723046b
Shubhamraj R Prasad <Shubhamraj4113@gmail.com>
2025-05-11 19:07:06 +0530

    Add files via upload

---
+++ React/http/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import PostList from './Components/PostList';
+import PostFrom from './Components/PostFrom';
+
+function App() {
+  return (
+    <div className="App">
+      <PostFrom />
+      {/* <PostList /> */}
+    </div>
+  );
+}
+
+export default App;
